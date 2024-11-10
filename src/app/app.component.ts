59312d17531afd6268f44c7b5cb0683749b7ff9d
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-11-10 19:27:40 +0100

    feat: add new articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,56 @@ import {RouterOutlet} from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {ArticleComponent} from './components/article/article.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, ArticleComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ArticleComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  prices: number[] = [150, 240, 650, 320];
+  titles: string[] = ['Vélo', 'TV', 'Jouet', 'Objet'];
+  message: string = "";
+
+  liste = [
+    {
+      titreArticle: "Vélo",
+      prixArticle: 80,
+      description: "Un super vélo tout terrain",
+      urlImg: "/images/velo.jpeg",
+      textAltImg: "Un vélo",
+      dispo: false
+    },
+    {
+      titreArticle: "TV",
+      prixArticle: 230,
+      description: "Très bonne qualité d'écran",
+      urlImg: "/images/tv.jpeg",
+      textAltImg: "Une télévision",
+      dispo: true
+    },
+    {
+      titreArticle: "Jouet",
+      prixArticle: 15,
+      description: "Neuf, jamais utilisé",
+      urlImg: "/images/jouet.jpeg",
+      textAltImg: "Un jouet pour enfant",
+      dispo: true
+    },
+    {
+      titreArticle: "TV",
+      prixArticle: 230,
+      description: "Très bonne qualité d'écran",
+      urlImg: "/images/tv.jpeg",
+      textAltImg: "Une télévision",
+      dispo: true
+    }
+  ]
+
+  onAffiche(arg: string) {
+    return this.message = "Merci d'avoir voté pour l'article : " + arg;
+  }
 }
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgIf, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgOptimizedImage,
+    NgIf
+  ],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
 export class ArticleComponent {
-  titreArticle: string = "Titre de l'article";
-  prixArticle: number = 12;
+  totalNbrLike: number = 0;
+  comment: string = "Ceci est un commentaire";
+  @Output() info = new EventEmitter<string>();
+
+  @Input() titreArticle!: string;
+  @Input() prixArticle!: number;
+  @Input() description!: string;
+  @Input() urlImg!: string;
+  @Input() textAltImg!: string;
+  @Input() dispo!: boolean;
+
+  onLike() {
+    this.totalNbrLike++;
+    this.info.emit(this.titreArticle);
+  }
 }
